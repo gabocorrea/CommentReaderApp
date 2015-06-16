efd1abd2e35ca7542f2457eb5cc0c45254efd4f9
gabriel correa <gabcorrea@gmail.com>
2015-06-16 14:54:54 -0300

    progress with weka text mining

--- normal_html_webpage/python-scripts/csv2arff_REAL_accepts_any_csv.py
+++ normal_html_webpage/python-scripts/csv2arff_REAL_accepts_any_csv.py
@@ -1,189 +1,60 @@
+"""Converts a .csv file to a .arff file. Output file has the same name as input file, but with the extension changed to .arff"""
+
+"""for now it only works for 2 columns: text,number       fix mysplit to make it work for more columns"""
+
 import re
 import csv,sys
 
+STRING_DELIMITER='¿'
 
 
-sys.argv
+def mysplit(astring, splitchar, maxsplit, ignoresplitcharacter):
+	p1=astring
+	p2=splitchar
+	p3=maxsplit
+	p4=ignoresplitcharacter
 
-if len(sys.argv)!=3:
-	print('\nUsage: csv2arff_REAL_accepts_any_csv.py <input.csv> <output.arff>\n')
+	L=[]
+	pos=p1.find(p4)
+	if pos<=-1:
+		return p1.split(p2,p3)
+	else:
+		pos2=p1.find(p4,pos+1)
+		L.append( p1[pos:pos2+1] )
+		L.append( p1[ p1.find(',',pos2) :  ] )
+		return L
+
+
+if len(sys.argv)<=1:
+	print('\nUsage: csv2arff_REAL_accepts_any_csv.py <input.csv> type1 type2 type3 ...\n')
 	sys.exit()
 
+with open(sys.argv[1]) as filein:
+	for line in filein:
+		firstLine = line
+		break
 
-num_of_columns = 6 #1st column should be id. last column should be comment_text
+	firstLineWords = firstLine.split(',')
+	num_of_columns = len(firstLineWords)
 
-sparce = False
+	firstLineWords[-1] = firstLineWords[-1][:-1] #take away the newline character
 
-dont_use_revise_directive_type = True  #if true, all revise setted values (in web app) are asumed to be directives
+	if num_of_columns!=len(sys.argv)-2:
+		print('\nlist of types in input must match number of columns of csv file')
+		print('Usage: csv2arff_REAL_accepts_any_csv.py <input.csv> type1 type2 type3 ...\n')
+		sys.exit()
 
 
-patterns = [r"\bcall\w*\b" ,
- r"\binvo\w*\b" ,
- r"\bbefore\b" ,
- r"\bafter\b" ,
- r"\bbetween\b" ,
- r"\bonce\b" ,
- r"\bprior\b" ,
- r"\bmust\b" ,
- r"\bmandat\w*\b" ,
- r"\brequire\w*\b" ,
- r"\bshall\b" ,
- r"\bshould\b" ,
- r"\bencourage\w*\b" ,
- r"\brecommend\w*\b" ,
- r"\bmay\b" ,
- r"\bassum\w*\b" ,
- r"\bonly\b" ,
- r"\bdebug\w*\b" ,
- r"\brestrict\w*\b" ,
- r"\bnever\b" ,
- r"\bcondition\w*\b" ,
- r"\bstrict\w*\b" ,
- r"\bnecessar\w*\b" ,
- r"\bportab\w*\b" ,
- r"\bstrong\w*\b" ,
- r"\bperforman\w*\b" ,
- r"\befficien\w*\b" ,
- r"\bfast\b" ,
- r"\bquick\b" ,
- r"\bbetter\b" ,
- r"\bbest\b" ,
- r"\bconcurren\w*\b" ,
- r"\bsynchron\w*\b" ,
- r"\block\w*\b" ,
- r"\bthread\w*\b" ,
- r"\bsimultaneous\w*\b" ,
- r"\bdesir\w*\b" ,
- r"\balternativ\w*\b" ,
- r"\baddition\w*\b" ,
- r"\bextend\w*\b" ,
- r"\boverrid\w*\b" ,
- r"\boverload\w*\b" ,
- r"\boverwrit\w*\b" ,
- r"\breimplement\w*\b" ,
- r"\bsubclass\w*\b" ,
- r"\bsuper\w*\b" ,
- r"\binherit\w*\b" ,
- r"\bwarn\w*\b" ,
- r"\baware\w*\b" ,
- r"\berror\w*\b" ,
- r"\bnote\w*\b"]
 
-class Debug:
-	def __init__(self):
-		self.NotUsed = True	
-	def log(self, text):
-		mode = "w" if self.NotUsed else "a"
-		with open("log.txt",mode) as fdlog:
-			fdlog.write(str(text))
-		if self.NotUsed: self.NotUsed=False
-debug = Debug()
+	with open(sys.argv[1][:-3]+'arff',"w") as fdout:
+		fdout.write("% 1. Title: arff file created from "+ sys.argv[1] +"\n\n")
+		fdout.write("@RELATION \""+sys.argv[1][:-3]+"arff\"\n\n")
 
-def getDirectiveKind(aStr):
-	if '1' in aStr:
-		return 'directive'
-	elif '0' in aStr:
-		return 'non-directive'
-	elif '2' in aStr:
-		return 'revise'
-	else:
-		return 1/0 #this should not happen
+		for i in range(num_of_columns):
+			fdout.write("@ATTRIBUTE "+firstLineWords[i]+ " "+ sys.argv[i+2]  +"\n")
+		fdout.write("\n@DATA\n")
 
-def getDirectiveKind_no_revise_type(aStr):
-	if '1' in aStr or '2' or '3' in aStr:
-		return 'directive'
-	elif '0' in aStr:
-		return 'non-directive'
-	else:
-		return 1/0 #this should not happen
 
-def getPatternCountList( text_original , regexList):
-	""" Returns a list of strings """
-	text=text_original
-	L=[]
-	for regex in regexList:
-		p = re.compile(regex, re.IGNORECASE)
-		
-		regexAns = p.findall(text)
-		if type(regexAns)==list:
-			if len(regexAns)<=0:
-				L.append(str(0))
-			else:
-				L.append(str(len(regexAns)))
-		else:
-			1/0 #should not happen
-	return L
-
-def getPatternCountListSparse( text, regexList):
-	""" Returns a list of strings """
-	L=[]
-	i=1
-	for regex in regexList:
-		p = re.compile(regex, re.IGNORECASE)
-
-		regexAns = p.findall(text)
-		if type(regexAns)==list:
-			if len(regexAns)<=0:
-				pass
-			else:
-				L.append(str(i)+' '+str(len(regexAns)))
-		else:
-			1/0 #should not happen
-
-		i+=1
-
-	return L
-
-sparce_str_file_name = ''
-if sparce:
-	sparce_str_file_name = '_(sparce)'
-if dont_use_revise_directive_type:
-	dont_use_revise_directive_type_name = '_(revise assumed to be directives)'
-with open(fileIn_name[:-4]+sparce_str_file_name+dont_use_revise_directive_type_name+"_1.arff","w") as fdout:
-	fdout.write("% 1. Title: Directive Keywords present in file "+ fileIn_name +"\n\n")
-	fdout.write("@RELATION directive_keywords_1\n\n")
-
-	fdout.write("@ATTRIBUTE id NUMERIC\n")
-	for elem in patterns:
-		inicio = elem.find("b")+1
-		fin = elem.find("\\", inicio)
-		word = elem[inicio:fin]
-		fdout.write("@ATTRIBUTE "+word+" NUMERIC\n")
-	if dont_use_revise_directive_type:
-		fdout.write("@ATTRIBUTE is_directive {directive, non-directive}\n")
-	else:
-		fdout.write("@ATTRIBUTE is_directive {directive, non-directive, revise}\n")
-	fdout.write("\n@DATA\n")
-
-	
-	with open(fileIn_name) as fdin:
-		csvDictReader = csv.DictReader(fdin)
-
-		i=0
-		for line in fdin:
-			if i==0:
-				i+=1
-				continue#skip 1st line
-
-			if i%5000==0:
-				print(str(i)+" lines scanned")
-			
-			row = line.split(',', num_of_columns-1)
-
-			p = re.compile(r'\d+')
-			if p.search(row[0]) == None:
-				debug.log('wrong id format in line '+str(i+1)+'\n')
-			else:
-				if not sparce:
-					if not dont_use_revise_directive_type:
-						fdout.write(row[0]+"," + ",".join(str(num) for num in getPatternCountList(str(row[num_of_columns-1]), patterns)) +","  +getDirectiveKind(row[2])+  "\n")
-					else:
-						fdout.write(row[0]+"," + ",".join(str(num) for num in getPatternCountList(str(row[num_of_columns-1]), patterns)) +","  +getDirectiveKind_no_revise_type(row[2])+  "\n")
-					
-				else:
-					if not dont_use_revise_directive_type:
-						fdout.write("{0 "+row[0]+","  +  ",".join(words for words in getPatternCountListSparse(str(row[num_of_columns-1]), patterns))  +str(len(patterns)+2-1)+" "+getDirectiveKind(row[2]) + "}\n")
-					else:
-						fdout.write("{0 "+row[0]+","  +  ",".join(words for words in getPatternCountListSparse(str(row[num_of_columns-1]), patterns))  +str(len(patterns)+2-1)+" "+getDirectiveKind_no_revise_type(row[2]) + "}\n")
-
-			i+=1
+		for line in filein:
+			### esto no es necesario ###row = mysplit(line,',', num_of_columns-1, STRING_DELIMITER)
+			fdout.write(line)
--- normal_html_webpage/python-scripts/datasetFormat_to_comment-class-csv.py
+++ normal_html_webpage/python-scripts/datasetFormat_to_comment-class-csv.py
@@ -1,3 +1,5 @@
+import re
+
 filename='dataset_corrected_only_comments_and_manually_revised_subset'
 fdout = open(filename+'_(only comments and class).csv','w')
 
@@ -10,7 +12,21 @@ with open(filename+'.csv') as fdin:
 			if i<=0:
 				fdout.write(splitted[5][:-1]+','+splitted[2]+'\n')
 			else:
-				fdout.write('¿'+splitted[5][:-1]+'¿,'+splitted[2]+'\n')
+				p=re.compile(r'"')
+				s=p.sub( r'\"', splitted[5])
+				s=s[:-1]
+				fdout.write('"'+s+'",')
+				if(splitted[2]=='0'):
+					fdout.write('non-directive\n')
+				elif(splitted[2]=='1'):
+					fdout.write('directive\n')
+				elif(splitted[2]=='2'):
+					fdout.write('directive\n')#('minor-directive\n')
+				elif(splitted[2]=='3'):
+					fdout.write('directive\n')#('null-directive\n')
+				else:
+					print('Error, read a number out of range in the directive types')
+					sys.exit()
 		else:
 			linesNotCorrectlyFormated += 1
 		i+=1
